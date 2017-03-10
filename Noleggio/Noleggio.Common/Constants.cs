7e32207e7627639f420f12626f041ecbd4dc3e89
Yankov <stoian_y@yahoo.com>
2017-03-10 17:27:28 +0200

    Added  Ststic class for constants

---
+++ Noleggio/Noleggio.Common/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Noleggio.Common
+{
+    public static class Constants
+    {
+        public const int UserMimiumAge = 18;
+        public const int UserMaximumAge = 110;
+    }
+}
