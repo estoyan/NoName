1f54d3cb1e49877d96890f050148b9182e9223a9
Yankov <stoian_y@yahoo.com>
2017-03-10 18:16:16 +0200

    Added BirthDateCalculator

---
+++ Noleggio/Noleggio.Common/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Noleggio.Common
+{
+    public class Calculator
+    {
+        public static byte? Age(DateTime? date)
+        {
+            if (!date.HasValue)
+            {
+                return null;
+            }
+
+            var birthDate = date.Value;
+            var now = DateTime.Now;
+
+            var age = now.Year - birthDate.Year;
+            if (now.Month < birthDate.Month || (now.Month == birthDate.Month && now.Day < birthDate.Day))
+            {
+                age--;
+            }
+
+            return (byte)age;
+        }
+    }
+}
