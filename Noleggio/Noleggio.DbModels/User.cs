7b3bbed27395d7575bb61ed1fae6da3bb90762ba
Yankov <stoian_y@yahoo.com>
2017-03-10 18:07:09 +0200

    Validation moved to Properties

--- Noleggio/Noleggio.DbModels/User.cs
+++ Noleggio/Noleggio.DbModels/User.cs
@@ -8,14 +8,24 @@ namespace Noleggio.DbModels
 {
     public class User
     {
-        
+
         const string ExceptionMiminumAgeMessage = "Mimimum allowed age is {0}";
-        const string ExceptionMaxinumAgeMessage ="Maximum allowed age is {0}";
+        const string ExceptionMaxinumAgeMessage = "Maximum allowed age is {0}";
+        const string FirstNameLengthExceptionMessage = "First Name should be between {0} and {1} symbols";
+        const string LastNameLengthExceptionMessage = "Last Name should be between {0} and {1} symbols";
+        const string CityLengthExceptionMessage = "City should be between {0} and {1} symbols";
+        const string AddressLengthExceptionMessage = "Address should be between {0} and {1} symbols";
 
         private ICollection<Comment> comments;
         private ICollection<Lease> leases;
         private ICollection<RentItem> items;
         private ICollection<Rating> ratings;
+
+        private string lastName;
+        private string firstName;
+        private int age;
+        private string city;
+        private string address;
 
         public User()
         {
@@ -31,11 +41,6 @@ namespace Noleggio.DbModels
         {
             Guard.WhenArgument(aspNetUserId, nameof(aspNetUserId)).IsEmptyGuid().Throw();
             Guard.WhenArgument(email, nameof(email)).IsNullOrEmpty().Throw();
-            Guard.WhenArgument(firstName, nameof(firstName)).IsNullOrEmpty().Throw();
-            Guard.WhenArgument(lastName, nameof(lastName)).IsNullOrEmpty().Throw();
-            Guard.WhenArgument(age, string.Format(ExceptionMiminumAgeMessage, Constants.UserMimiumAge)).IsLessThan(Constants.UserMimiumAge).Throw();
-            Guard.WhenArgument(age, string.Format(ExceptionMaxinumAgeMessage, Constants.UserMaximumAge)).IsGreaterThan(Constants.UserMaximumAge).Throw();
-            Guard.WhenArgument(city, nameof(city)).IsNullOrEmpty().Throw();
 
             this.Id = aspNetUserId;
             this.Username = email;
@@ -45,26 +50,103 @@ namespace Noleggio.DbModels
             this.Age = age;
         }
 
-        public Guid Id { get; set; }
+        public Guid Id { get; private set; }
 
         [Required]
         [EmailAddress]
-        public string Username { get; set; }
+        public string Username { get; private set; }
 
         [Required]
-        public string FirstName { get; set; }
+        [StringLength(Constants.UserFirstNameMaximumLenght, MinimumLength = Constants.UserClassMinimumStringLenght)]
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+
+            set
+            {
+                Guard.WhenArgument(value, nameof(firstName)).IsNullOrEmpty().Throw();
+                Guard.WhenArgument(value.Length, string.Format(FirstNameLengthExceptionMessage, Constants.UserClassMinimumStringLenght, Constants.UserFirstNameMaximumLenght))
+                     .IsLessThan(Constants.UserClassMinimumStringLenght)
+                     .IsGreaterThan(Constants.UserFirstNameMaximumLenght).Throw();
+                this.firstName = value;
+            }
+        }
 
         [Required]
-        public string LastName { get; set; }
+        [StringLength(Constants.UserLastNameMaximumLenght, MinimumLength = Constants.UserClassMinimumStringLenght)]
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+            set
+            {
+                Guard.WhenArgument(value, nameof(LastName)).IsNullOrEmpty().Throw();
+                Guard.WhenArgument(value.Length, string.Format(LastNameLengthExceptionMessage, Constants.UserClassMinimumStringLenght, Constants.UserLastNameMaximumLenght))
+                   .IsLessThan(Constants.UserClassMinimumStringLenght)
+                   .IsGreaterThan(Constants.UserLastNameMaximumLenght).Throw();
+                this.lastName = value;
+            }
+        }
 
         [Required]
-        public string City { get; set; }
+        [StringLength(Constants.UserCityMaximumLength, MinimumLength = Constants.UserClassMinimumStringLenght)]
+        public string City
+        {
+            get
+            {
+                return this.city;
+            }
+
+            set
+            {
+                Guard.WhenArgument(value, nameof(city)).IsNullOrEmpty().Throw();
+                Guard.WhenArgument(value.Length, string.Format(CityLengthExceptionMessage, Constants.UserClassMinimumStringLenght, Constants.UserCityMaximumLength))
+                  .IsLessThan(Constants.UserClassMinimumStringLenght)
+                  .IsGreaterThan(Constants.UserCityMaximumLength).Throw();
+                this.city = value;
+            }
+        }
 
         [Required]
-        public string Address { get; set; }
+        [StringLength(Constants.UserAddressMaximumLength, MinimumLength = Constants.UserClassMinimumStringLenght)]
+        public string Address
+        {
+            get
+            {
+                return this.address;
+            }
+
+            set
+            {
+                Guard.WhenArgument(value, nameof(address)).IsNullOrEmpty().Throw();
+                Guard.WhenArgument(value.Length, string.Format(AddressLengthExceptionMessage, Constants.UserClassMinimumStringLenght, Constants.UserAddressMaximumLength))
+                  .IsLessThan(Constants.UserClassMinimumStringLenght)
+                  .IsGreaterThan(Constants.UserAddressMaximumLength).Throw();
+
+                this.address = value;
+            }
+        }
 
         [Required]
-        public int Age { get; set; }
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+
+            set
+            {
+                Guard.WhenArgument(value, string.Format(ExceptionMiminumAgeMessage, Constants.UserMimiumAge)).IsLessThan(Constants.UserMimiumAge).Throw();
+                Guard.WhenArgument(value, string.Format(ExceptionMaxinumAgeMessage, Constants.UserMaximumAge)).IsGreaterThan(Constants.UserMaximumAge).Throw();
+                this.age = value;
+            }
+        }
 
         //[Required]
         //public string Email { get; set; }
