0c1ed72c5467e00ef1077b6a5eb9095e421e805e
Yankov <stoian_y@yahoo.com>
2017-03-11 21:41:49 +0200

    Lease tests added

---
+++ Noleggio/Noleggio.DBModels.Tests/LeaseTests/Constructor_Should.cs
@@ -0,0 +1,50 @@
+﻿using Noleggio.DbModels;
+using NUnit.Framework;
+using Ploeh.AutoFixture;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Noleggio.DBModels.Tests.LeaseTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void ReturnNewInstance()
+        {
+            //Arrange
+            var fixture = new Fixture();
+            var item = fixture.Create<Guid>();
+            var user = fixture.Create<Guid>();
+            var startDate = new DateTime(2017, 03, 11);
+            var endDate = new DateTime(2017,04,11);
+
+            //Act 
+            var lease = new Lease(item, user, startDate, endDate);
+
+            //Assert
+            Assert.IsNotNull(lease);
+        }
+
+        [Test]
+        public void ReturnNewInstanceOfLease()
+        {
+            //Arrange
+            var fixture = new Fixture();
+            var item = fixture.Create<Guid>();
+            var user = fixture.Create<Guid>();
+            var startDate = new DateTime(2017, 03, 11);
+            var endDate = new DateTime(2017, 04, 11);
+
+            //Act 
+            var lease = new Lease(item, user, startDate, endDate);
+
+            //Assert
+            Assert.IsInstanceOf<Lease>(lease);
+        }
+
+    }
+}
---
+++ Noleggio/Noleggio.DBModels.Tests/LeaseTests/Constructor_Should_Throw.cs
@@ -0,0 +1,57 @@
+﻿using Noleggio.DbModels;
+using NUnit.Framework;
+using Ploeh.AutoFixture;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Noleggio.DBModels.Tests.LeaseTests
+{
+    [TestFixture]
+    public class Constructor_Should_Throw
+    {
+        [Test]
+        public void WhenRentItemIsEmptyGuid()
+        {
+            //Arrange
+            var fixture = new Fixture();
+            var item = new Guid();
+            var user = fixture.Create<Guid>();
+            var startDate = new DateTime(2017, 03, 11);
+            var endDate = new DateTime(2017, 04, 11);
+
+            //Act &Assert
+            Assert.Throws<ArgumentException>(()=> new Lease(item, user, startDate, endDate));
+        }
+
+        [Test]
+        public void WhenUserIsEmptyGuid()
+        {
+            //Arrange
+            var fixture = new Fixture();
+            var item = fixture.Create<Guid>();
+            var user = new Guid(); 
+            var startDate = new DateTime(2017, 03, 11);
+            var endDate = new DateTime(2017, 04, 11);
+
+            //Act &Assert
+            Assert.Throws<ArgumentException>(() => new Lease(item, user, startDate, endDate));
+        }
+
+        [Test]
+        public void WhenEndDateIsBeforeStartDate()
+        {
+            //Arrange
+            var fixture = new Fixture();
+            var item = fixture.Create<Guid>();
+            var user = fixture.Create<Guid>();
+            var startDate = new DateTime(2017, 03, 11);
+            var endDate = new DateTime(2017, 02, 11);
+
+            //Act &Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Lease(item, user, startDate, endDate));
+        }
+    }
+}
