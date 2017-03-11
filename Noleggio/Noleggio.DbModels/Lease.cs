09df36fb255a800264a6fd473e267e1f87dd4c24
Yankov <stoian_y@yahoo.com>
2017-03-11 21:26:50 +0200

    Constructor changed to accept Id's instead of whole objects

--- Noleggio/Noleggio.DbModels/Lease.cs
+++ Noleggio/Noleggio.DbModels/Lease.cs
@@ -1,31 +1,26 @@
 ﻿using Bytes2you.Validation;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Noleggio.DbModels
 {
     public class Lease
     {
-        //TODO extract all exceptions in a  separete file
         const string StartDateCannobeGreater = "Start Date cannot be greater than end date";
 
         public Lease()
         {
         }
 
-        public Lease(RentItem item, User user, DateTime startDate, DateTime endDate) : this()
+        public Lease(Guid rentItem, Guid user, DateTime startDate, DateTime endDate) : this()
         {
-            Guard.WhenArgument(item, nameof(item)).IsNull().Throw();
-            Guard.WhenArgument(user, nameof(user)).IsNull().Throw();
+            Guard.WhenArgument(rentItem, nameof(rentItem)).IsEmptyGuid().Throw();
+            Guard.WhenArgument(user, nameof(user)).IsEmptyGuid().Throw();
             Guard.WhenArgument(startDate, StartDateCannobeGreater).IsLessThanOrEqual(endDate).Throw();
 
-            this.Item = item;
-            this.User = user;
+            this.ItemId = rentItem;
+            this.UserId = user;
             this.StartDate = startDate;
             this.EndDate = endDate;
         }
@@ -40,11 +35,11 @@ namespace Noleggio.DbModels
         public DateTime EndDate { get; private set; }
 
         [Required]
-        public Guid ItemId { get; set; }
+        public Guid ItemId { get; private set; }
         public virtual RentItem Item { get; set; }
 
         [Required]
-        public Guid UserId { get; set; }
+        public Guid UserId { get; private set; }
         public virtual User User { get; set; }
 
     }
