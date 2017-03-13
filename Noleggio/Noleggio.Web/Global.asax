2abe8862dd4eb896cd5b4305a36842224e8e46ed
Yankov <stoian_y@yahoo.com>
2017-03-13 13:03:57 +0200

    Added MVC Web Solution

---
+++ Noleggio/Noleggio.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Noleggio.Web.MvcApplication" Language="C#" %>
---
+++ Noleggio/Noleggio.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Noleggio.Web.Startup))]
+namespace Noleggio.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
