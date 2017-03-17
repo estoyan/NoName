95cc66d72aef904c8c814f5f8fb1441d1edbe035
Yankov <stoian_y@yahoo.com>
2017-03-17 12:08:08 +0200

    Added Service Assembly

---
+++ Noleggio/Noleggio.Services/Contracts/INolegioGenericService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Noleggio.Services.Contracts
+{
+    public interface INolegioGenericService<T>
+    {
+
+        T GetById(object id);
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Hide(T entity);
+
+        void Delete(T entity);
+
+        IQueryable<T> GetAll();
+
+        IQueryable<T> GetAll(Expression<Func<T, bool>> filter);
+
+        IQueryable<T> GetAll<T1>(
+           Expression<Func<T, bool>> filter,
+           Expression<Func<T, T1>> orderBy);
+
+        IQueryable<TResult> GetAll<T1, TResult>(
+           Expression<Func<T, bool>> filter,
+           Expression<Func<T, T1>> orderBy,
+           Expression<Func<T, TResult>> select);
+
+        IQueryable<T> GetDeleted();
+    }
+}
---
+++ Noleggio/Noleggio.Services/NoleggioGenericService.cs
@@ -0,0 +1,158 @@
+﻿using Noleggio.DbModels;
+using Noleggio.Services.Contracts;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using Noleggio.Data.Contracts;
+
+namespace Noleggio.Services
+{
+    public abstract class NoleggioGenericService<TEntity> : INolegioGenericService<TEntity> where TEntity : class, IDeletableEntity
+    {
+
+        private readonly IGenericEfRepository<TEntity> repository;
+        private readonly IUnitOfWork unitOfWork;
+
+        public NoleggioGenericService(IGenericEfRepository<TEntity> repository, IUnitOfWork unitOfWork)
+        {
+            if (repository == null)
+            {
+                throw new ArgumentNullException("Repository cannot be null!");
+            }
+
+            if (unitOfWork == null)
+            {
+                throw new ArgumentNullException("UnitOfWork cannot be null!");
+            }
+
+            this.repository = repository;
+            this.unitOfWork = unitOfWork;
+        }
+
+        protected IUnitOfWork UnitOfWork
+        {
+            get
+            {
+                return this.unitOfWork;
+            }
+        }
+
+        protected IGenericEfRepository<TEntity> Repository
+        {
+            get
+            {
+                return this.repository;
+            }
+        }
+
+        public void Add(TEntity entity)
+        {
+            this.repository.Add(entity);
+
+            using (this.UnitOfWork)
+            {
+                this.UnitOfWork.Commit();
+            }
+        }
+
+        public void Update(TEntity entity)
+        {
+            this.repository.Update(entity);
+
+            using (this.UnitOfWork)
+            {
+                this.UnitOfWork.Commit();
+            }
+        }
+
+        public void Hide(TEntity entity)
+        {
+            this.repository.Hide(entity);
+
+            using (this.UnitOfWork)
+            {
+                this.UnitOfWork.Commit();
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+            this.repository.Delete(entity);
+
+            using (this.UnitOfWork)
+            {
+                this.UnitOfWork.Commit();
+            }
+        }
+
+        public TEntity GetById(object id)
+        {
+            if ((int)id < 0)
+            {
+                throw new ArgumentNullException("Id can't be negative!");
+            }
+
+            if (id == null)
+            {
+                throw new ArgumentNullException("Id can't be null!");
+            }
+
+            return this.repository.GetById(id);
+        }
+
+        public IQueryable<TEntity> GetAll()
+        {
+            return this.repository.GetAll();
+        }
+
+        public IQueryable<TEntity> GetDeleted()
+        {
+            return this.GetAll(ent => ent.IsDeleted);
+        }
+
+        public IQueryable<TEntity> GetAll(Expression<Func<TEntity, bool>> filter)
+        {
+            if (filter == null)
+            {
+                throw new ArgumentNullException("Filter can't be null");
+            }
+
+            return this.repository.GetAll(filter);
+        }
+
+        public IQueryable<TEntity> GetAll<T1>(Expression<Func<TEntity, bool>> filter, Expression<Func<TEntity, T1>> orderBy)
+        {
+            if (filter == null)
+            {
+                throw new ArgumentNullException("Filter can't be null");
+            }
+
+            if (orderBy == null)
+            {
+                throw new ArgumentNullException("OrderBy can't be null");
+            }
+
+            return this.repository.GetAll(filter, orderBy);
+        }
+
+        public IQueryable<TResult> GetAll<T1, TResult>(Expression<Func<TEntity, bool>> filter, Expression<Func<TEntity, T1>> orderBy, Expression<Func<TEntity, TResult>> select)
+        {
+            if (filter == null)
+            {
+                throw new ArgumentNullException("Filter can't be null");
+            }
+
+            if (orderBy == null)
+            {
+                throw new ArgumentNullException("OrderBy can't be null");
+            }
+
+            if (select == null)
+            {
+                throw new ArgumentNullException("Select can't be null");
+            }
+
+            return this.repository.GetAll(filter, orderBy, select);
+        }
+    }
+}
