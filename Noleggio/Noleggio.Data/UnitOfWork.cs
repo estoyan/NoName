93fbc64d7ae2b0a6abd0cc2bc4c6927a849bf15f
Yankov <stoian_y@yahoo.com>
2017-03-17 12:07:42 +0200

    UnitOfWork Implemented

---
+++ Noleggio/Noleggio.Data/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using Noleggio.Data.Contracts;
+using System;
+
+namespace Noleggio.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly INoleggioDbContext dbContext;
+
+        public UnitOfWork(INoleggioDbContext dbContext)
+        {
+            if (dbContext == null)
+            {
+                throw new ArgumentNullException("DbContext can't be null");
+            }
+
+            this.dbContext = dbContext;
+        }
+
+        public void Commit()
+        {
+            this.dbContext.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
