588c6f536f557c8ac2a44424efe5b80ceec09f4e
Yankov <stoian_y@yahoo.com>
2017-03-20 21:32:47 +0200

    Added Aditional tests

---
+++ Noleggio/Tests/Noleggio.Common.Tests/AgeCalculatorTests/AgeShould.cs
@@ -0,0 +1,24 @@
+﻿using Noleggio.Common;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Noleggio.Common.Tests.AgeCalculatorTests
+{
+    [TestFixture]
+    public class AgeShould
+    {
+        [Test]
+        public void REturnsNullIfDateIsValid()
+        {
+            //Arrange & Act & Assert
+
+            Assert.AreEqual(null, AgeCalculator.Age(null));
+        }
+    }
+
+    //TODO mock DateTIme
+}
--- Noleggio/Tests/Noleggio.Services.Tests/NoleggioGenericServiceTests/ProtectedProperty_RepositoryShould.cs
+++ Noleggio/Tests/Noleggio.Services.Tests/NoleggioGenericServiceTests/ProtectedProperty_RepositoryShould.cs
@@ -2,11 +2,6 @@
 using Noleggio.Data.Contracts;
 using Noleggio.DbModels;
 using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Noleggio.Services.Tests.NoleggioGenericServiceTests
 {
